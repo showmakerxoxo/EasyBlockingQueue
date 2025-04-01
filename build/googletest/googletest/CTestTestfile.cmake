b2a1e931d630b82f07cb4489b5c1fee40cc6f71d
showmakerxoxo <reall_ss@163.com>
2025-04-01 21:53:32 +0800

    delete useless files

--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/showmaker/gitspace/EasyBlockingQueue/googletest/googletest
-# Build directory: /Users/showmaker/gitspace/EasyBlockingQueue/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
